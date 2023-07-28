6a8b11fafdbfce2ec69765931bed4d9eb6055533
1379255913 <1379255913@qq.com>
2023-07-29 00:57:11 +0800

    feat: 测试github action

--- config/config.prod.js
+++ config/config.prod.js
@@ -10,16 +10,24 @@ module.exports = app => {
   const config = {}
 
   config.sequelize = {
-    username: 'root',
-    password: '',
-    database: 'template_node_egg',
+    // 连接用户
+    username: 'node_egg',
+    // 连接密码
+    password: 'JHw2xb4BMfJkD3rZ',
+    // 连接的数据库，可根据需要改成已有的数据库
+    database: 'node_egg',
+    // 连接地址
     host: '127.0.0.1',
+    // 数据库类型
     dialect: 'mysql'
   }
 
-  // 生产环境跨域配置
-  config.cors = {
-    origin: ''
+  config.cluster = {
+    listen: {
+      path: '',
+      port: 7001,
+      hostname: '0.0.0.0'
+    }
   }
 
   // 自定义日志路径
